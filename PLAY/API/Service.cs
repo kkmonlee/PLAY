71921bf8ebe2fd0c0f5c479d0ecad90ecc886d30
kkmonlee <kkmonlee@gmail.com>
2015-05-24 05:55:10 +0100

    Stuff

---
+++ PLAY/API/Service.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace PLAY.API
+{
+    class Service : IDisposable
+    {
+        private TcpListener tcpListener;
+        private Thread listenThread;
+        private List<TcpClient> Clients;
+        private bool isListening;
+
+        public Service()
+        {
+            tcpListener = new TcpListener(IPAddress.Any, 3000);
+            Clients = new List<TcpClient>();
+        }
+
+        public void Start()
+        {
+            listenThread = new Thread(new ThreadStart(ListenForClients));
+            listenThread.Start();
+            isListening = true;
+        }
+
+        private void ListenForClients()
+        {
+            tcpListener.Start();
+            while (isListening)
+            {
+                try
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    Clients.Add(client);
+                    Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                    clientThread.Start(client);
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+        }
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient) client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            BinaryReader reader = new BinaryReader(clientStream);
+            BinaryWriter writer = new BinaryWriter(clientStream);
+
+            int BufferSize;
+
+            while (true)
+            {
+                try
+                {
+                    BufferSize = reader.ReadInt32();
+                }
+                catch
+                {
+                    break;
+                }
+
+                byte[] buffer = reader.ReadBytes(BufferSize);
+                string command = System.Text.Encoding.ASCII.GetString(buffer);
+                string response = string.Empty;
+
+                switch (command)
+                {
+                    case "gh":
+                        Application.Current.Dispatcher.Invoke(
+                            () => response = new WindowInteropHelper(Application.Current.MainWindow).Handle.ToString());
+                        break;
+                        
+                }
+
+                byte[] responsebuffer = System.Text.Encoding.ASCII.GetBytes(response);
+                writer.Write(responsebuffer.Length);
+                writer.Write(responsebuffer);
+            }
+
+            tcpClient.Close();
+        }
+
+
+
+        public void Dispose()
+        {
+            foreach (TcpClient c in Clients)
+            {
+                c.Close();
+            }
+            tcpListener.Stop();
+            isListening = false;
+        }
+    }
+}
---
+++ PLAY/Extensions/ButtonHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PLAY.Extensions
+{
+    public class ButtonHelper
+    {
+        public static bool? GetDialogResult(DependencyObject obj)
+        {
+            return (bool?) obj.GetValue(DialogResultProperty);
+        }
+
+        public static void SetDialogResult(DependencyObject obj, bool? value)
+        {
+            obj.SetValue(DialogResultProperty, value);
+        }
+
+        public static readonly DependencyProperty DialogResultProperty =
+            DependencyProperty.RegisterAttached("DialogResult", typeof (bool?), typeof (ButtonHelper),
+                new UIPropertyMetadata
+                {
+                    PropertyChangedCallback = (obj, e) =>
+                    {
+                        Button button = obj as Button;
+                        if (button == null)
+                            throw new InvalidOperationException("DialogResult is working, but not other controls.");
+                        button.Click += (sender, e2) =>
+                        {
+                            Window.GetWindow(button).DialogResult = GetDialogResult(button);
+                        };
+                    }
+                });
+    }
+}
